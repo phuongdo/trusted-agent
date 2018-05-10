d86253851c8aabf945a1e5aaf281beb63cbd3b32
phuongdv <dovietphuong.ptit@gmail.com>
2018-05-10 16:11:23 +0700

    example

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # Trusted Agent
 
-
+```
 curl -sSL https://raw.githubusercontent.com/phuongdo/trusted-agent/master/scripts/registerNode.sh | bash
+```
+
+
+
+How to ssh
+
+https://superuser.com/questions/125324/how-can-i-avoid-sshs-host-verification-for-known-hosts
+
+```
+ssh -o "StrictHostKeyChecking no" user@host
+
+```
--- scripts/registerNode.sh
+++ scripts/registerNode.sh
@@ -6,6 +6,6 @@ if [ ! -d .ssh ]; then
   mkdir -p .ssh
 fi
 
-cat https://raw.githubusercontent.com/phuongdo/trusted-agent/master/ssh/id_rsa.sh | 'cat >> $HOME/.ssh/authorized_keys'
+curl -sSL https://raw.githubusercontent.com/phuongdo/trusted-agent/master/ssh/id_rsa.pub | cat  >> $HOME/.ssh/authorized_keys
 chmod +700 $HOME/.ssh
 chmod +640 $HOME/.ssh/authorized_keys
